f890e7e9ddfe1bc6acf636d4c658c927ec09f872
badreddine <killerkillersss2000@gmail.com>
2025-09-18 11:50:14 +0100

    this is a chapter 12 when we do the exercise of C1 C2 C3 here

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-for (let i = 1; i <= 10; i++) {
-    if (i === 5) continue; // sauter le 5
-    console.log("Nombre : " + i);
+let Values = [ "C1" , "C1" , "C2" , "C3" , "C2" ] ;
+
+
+let things =["C1", "C2" , "C3"] ;
+
+let count = 0 ;
+
+let i = 0 ;
+while ( i < things.length ) 
+{
+  let j = 0 ;
+  while( j < Values.length )
+  {
+     if(things[i] === Values[j] )
+     {
+       count++;
+     }
+     j++;
   }
-  
+   console.log( things[i] +" has repeated for " + count + " times " );
+   console.log("\n");
+   count = 0 ;
+  i++; 
+}
